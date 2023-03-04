937302133af6442be334252c90e3db1552d68da8
Roman Danylenko <ramonvicious@gmail.com>
2023-03-04 17:35:29 +0300

    Initial commit

---
+++ app/src/main/java/ua/edu/sumdu/droidcage/OrderActivity.java
@@ -0,0 +1,13 @@
+package ua.edu.sumdu.droidcage;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
